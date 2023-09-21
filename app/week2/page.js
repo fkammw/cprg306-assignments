731d619992ce62dec045f7a37631d65a45ba300f
fkammw <123600013+fkammw@users.noreply.github.com>
2023-09-21 17:04:10 -0600

    Update week2/page.js

--- app/week2/page.js
+++ app/week2/page.js
@@ -4,7 +4,7 @@ export default function Page(){
     return(
         <div>
             <h1 className="text-left font-bold text-4xl m-4">My Shopping List</h1>
-            <p className="text-right"><StudentInfo>Info</StudentInfo></p>
+            <StudentInfo />
         </div>
     );
 }
